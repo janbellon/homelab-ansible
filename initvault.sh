3c48169f2609b8f09ffa62f7a434a22b65b8ebcf
root <root@tf-ansible-code.enpos.lan>
2025-11-28 14:35:41 +0000

    ACME fixing & Gitlab Update

--- initvault.sh
+++ initvault.sh
@@ -1,5 +1,5 @@
 export VAULT_TOKEN=$1
 export VAULT_ADDR="https://vault.enpos.lan"
 export NETBOX_URL="https://netbox.enpos.lan"
-export NETBOX_TOKEN=$(vault kv get -field netbox_token kv/ansible)
+export NETBOX_TOKEN=$(vault kv get -tls-skip-verify -field netbox_token kv/ansible)
 export SSL_CERT_FILE
