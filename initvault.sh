f0551eef5ef81b95e55128c513c5bd08ae4f76a4
Jan Bellon <jan@nobell.fr>
2025-12-17 23:47:28 +0000

    Update code for Nobell ACME server

--- initvault.sh
+++ initvault.sh
@@ -1,5 +1,5 @@
 export VAULT_TOKEN=$1
-export VAULT_ADDR="https://vault.enpos.lan"
-export NETBOX_URL="https://netbox.enpos.lan"
-export NETBOX_TOKEN=$(vault kv get -tls-skip-verify -field netbox_token kv/ansible)
-export SSL_CERT_FILE
+export VAULT_ADDR="https://vault.srv.nbl.lan"
+export NETBOX_URL="https://netbox.srv.nbl.lan"
+export NETBOX_TOKEN=$(vault kv get -tls-skip-verify -field netbox_api_key kv/ansible)
+export VAULT_SKIP_VERIFY=true
